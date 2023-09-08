f0414ceb7df75e72cc5a02895bd4d5667087464b
kaiiam <kblumberg@email.arizona.edu>
2023-09-08 14:58:52 +0200

    Add test data

--- data_validation/run_examples.sh
+++ data_validation/run_examples.sh
@@ -9,3 +9,4 @@ linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S foods test_data/
 
 ## Components
 linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-001.tsv
+linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-002.tsv
