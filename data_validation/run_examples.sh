834ce5e0130083d40f299c8b510c74884119360f
kaiiam <kblumberg@email.arizona.edu>
2023-09-08 14:54:51 +0200

    Add script to run test data examples

---
+++ data_validation/run_examples.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -u
+
+# Validate example data
+
+## Foods
+linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S foods test_data/Food-001.tsv
+
+## Components
+linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-001.tsv
