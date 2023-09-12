87a74227e7a39adf32e9f36f3b20b1f722ffd886
kaiiam <kblumberg@email.arizona.edu>
2023-09-12 17:30:21 +0200

    Add component 3 example from UMD interium Foundation Food data

--- data_validation/run_examples.sh
+++ data_validation/run_examples.sh
@@ -11,3 +11,4 @@ linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S foods test_data/
 ## Components
 linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-001.tsv
 linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-002.tsv
+linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-003.tsv
