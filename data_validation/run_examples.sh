2d96503871c1ace912ea35ae8ef96df6b26e0015
kaiiam <kblumberg@email.arizona.edu>
2024-06-05 18:21:48 +0200

    Add line to validate example Provenance tsv

--- data_validation/run_examples.sh
+++ data_validation/run_examples.sh
@@ -12,3 +12,6 @@ linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S foods test_data/
 linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-001.tsv
 linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-002.tsv
 linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S components test_data/Component-003.tsv
+
+##Provenance
+linkml-validate -s ../src/mifc/schema/mifc.yaml -C Container -S provenances test_data/Provenance-001.tsv
