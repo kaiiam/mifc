002f1e860b1bca4e91d0ae982141f0572fbd941e
Mark A. Miller <MAM@lbl.gov>
2024-06-12 11:03:40 -0400

    restored valid preservation to valid example

--- project.Makefile
+++ project.Makefile
@@ -1,8 +1,10 @@
 ## Add your own custom Makefile targets here
 
-.PHONY: clean-examples clean-examples
+.PHONY: clean-examples clean-examples all-all
 
 clean-examples:
 	rm -rf examples/output
 
 examples-all: clean-examples examples/output
+
+all-all: clean all test examples-all
--- tests/test_data.py
+++ tests/test_data.py
@@ -7,7 +7,7 @@ from linkml_runtime.loaders import yaml_loader
 from mifc.datamodel.mifc import Container
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
-DATA_DIR = os.path.join(ROOT, "src", "data", "examples")
+DATA_DIR = os.path.join(ROOT, "src", "data", "examples", "valid")
 
 EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR, '*.yaml'))
 
