4b110fb723081c4358533fa9bbe2651e396a3f0a
kaiiam <kblumberg@email.arizona.edu>
2024-06-20 13:51:18 +0200

    Run make all-all

--- src/mifc/datamodel/mifc.py
+++ src/mifc/datamodel/mifc.py
@@ -1,5 +1,5 @@
 # Auto generated from mifc.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-06-12T11:37:06
+# Generation date: 2024-06-20T13:50:21
 # Schema: mifc
 #
 # id: https://w3id.org/kaiiam/mifc
